c89baedf29f5dfdf45ab115a29217ea82a8e0547
ankush <ankushsaha4321@gmail.com>
2023-07-29 23:10:27 +0530

    changes

--- uni.js
+++ uni.js
@@ -1,24 +1,50 @@
-const name = "";
-const country = "india";
+// const name = "";
+// const country = "india";
 
+// const input = document.querySelector(".input");
+// input.onkeyup = ()=>{
+//    university(input.value);
+// }
+// async function university(text){
+//     const response = await fetch(`http://universities.hipolabs.com/search?name=${text}&country=india`)
+//     const data = await response.json();
+//     const suggest = document.querySelector(".suggest");
+//     arr = [];
+//     if(input.value){
+//     arr = data;
+//     newarr = arr.map((ele)=>{
+//         return `<div onclick = fun(this)>${ele.name}</div>`
+//     })
+//     suggest.innerHTML = newarr ;
+// }else{
+//     suggest.innerHTML = '';
+// }
+// }
+// function fun(var_){
+//     input.value = var_.innerHTML;
+// }
+const country = "india";
 const input = document.querySelector(".input");
-input.onkeyup = ()=>{
+input.onkeyup = ()=>{ 
    university(input.value);
+   console.log(input.value.length);
 }
+const suggest = document.querySelector(".suggest");
 async function university(text){
     const response = await fetch(`http://universities.hipolabs.com/search?name=${text}&country=india`)
     const data = await response.json();
-    const suggest = document.querySelector(".suggest");
     arr = [];
-    if(input.value){
+    if(input.value.length){  
     arr = data;
-    newarr = arr.map((ele)=>{
-        return `<div onclick = fun(this)>${ele.name}</div>`
-        // += is string that's why it wasn't working
-    })
-    suggest.innerHTML = newarr ;
+    arr.forEach((element,index) => {
+        ele = `<div onclick = fun(this)>${element.name}</div>`;
+        arr[index] = ele;
+    });
+    suggest.innerHTML = arr ;
+    document.getElementById("_suggest").style.display = "flex"; 
 }else{
-    suggest.innerHTML = '';
+    suggest.innerHTML = "";
+    document.getElementById("_suggest").style.display = "none";
 }
 }
 function fun(var_){
